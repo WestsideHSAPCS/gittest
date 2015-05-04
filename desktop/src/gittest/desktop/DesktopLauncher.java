ab6519b6860c651691607d7f5fd494ecc955cc21
stejskal.ryan <stejskal.ryan@whs-stejskal>
2015-05-04 00:17:07 -0500

    Add git to project

---
+++ desktop/src/gittest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gittest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gittest.GitTestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GitTestGame(), config);
+	}
+}
